2880c89d4756948ca45a296e1f3b7ba38833027d
Sean Aye <hello@seanaye.ca>
2022-06-11 20:38:21 -0400

    add deno adapter

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind()]
+  integrations: [solid(), tailwind()],
+  adapter: deno(),
 });
